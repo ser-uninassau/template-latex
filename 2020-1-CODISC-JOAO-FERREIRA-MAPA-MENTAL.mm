861ca85dd4375cc785452f0496aa553140533cc1
João Ferreira <joaoferreirape@gmail.com>
2021-09-12 20:45:56 -0300

    Extensas modificações na estrutura, atualização para dados de 2021, criação de novas seções e adicionado o mapa mental.

---
+++ 2020-1-CODISC-JOAO-FERREIRA-MAPA-MENTAL.mm
@@ -0,0 +1,249 @@
+<map version="1.1.0">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node COLOR="#000000" CREATED="1631469347218" ID="ID_660139729" MODIFIED="1631469380390" TEXT="Modelo">
+<font NAME="SansSerif" SIZE="20"/>
+<hook NAME="accessories/plugins/AutomaticLayout.properties"/>
+<node COLOR="#0033ff" CREATED="1631469384175" ID="ID_768294769" MODIFIED="1631490126299" POSITION="right" TEXT="Abertura">
+<edge STYLE="sharp_bezier" WIDTH="8"/>
+<font NAME="SansSerif" SIZE="18"/>
+<node COLOR="#00b439" CREATED="1631469512829" ID="ID_1063458929" MODIFIED="1631469514492" TEXT="Slide 1">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+<node COLOR="#990000" CREATED="1631469403125" ID="ID_225068300" MODIFIED="1631469517250" TEXT="UNINASSAU imagem banner">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+</node>
+<node COLOR="#00b439" CREATED="1631469507228" ID="ID_1554272869" MODIFIED="1631469511551" TEXT="Slide 2">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+<node COLOR="#990000" CREATED="1631469403125" ID="ID_1331088225" MODIFIED="1631469531784" TEXT="UNINASSAU imagem banner desfocada">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+<node COLOR="#990000" CREATED="1631469419107" ID="ID_895331835" MODIFIED="1631469520296" TEXT="NOME DO CURSO">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+<node COLOR="#990000" CREATED="1631469424947" ID="ID_1688378499" MODIFIED="1631469520303" TEXT="NOME DA DISCIPLINA">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+<node COLOR="#990000" CREATED="1631469431216" ID="ID_1307885573" MODIFIED="1631469520307" TEXT="NOME DO PROFESSOR">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+</node>
+</node>
+<node COLOR="#0033ff" CREATED="1631469368475" ID="ID_316699592" MODIFIED="1631490125460" POSITION="right" TEXT="Agenda">
+<edge STYLE="sharp_bezier" WIDTH="8"/>
+<font NAME="SansSerif" SIZE="18"/>
+<node COLOR="#00b439" CREATED="1631469571251" ID="ID_912845504" MODIFIED="1631469573153" TEXT="Se&#xe7;&#xe3;o">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+<node COLOR="#990000" CREATED="1631469574780" ID="ID_957367053" MODIFIED="1631469576994" TEXT="Sub-se&#xe7;&#xe3;o">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+</node>
+</node>
+<node COLOR="#0033ff" CREATED="1631470402544" ID="ID_7667073" MODIFIED="1631490124429" POSITION="right" TEXT="Professor">
+<edge STYLE="sharp_bezier" WIDTH="8"/>
+<font NAME="SansSerif" SIZE="18"/>
+<node COLOR="#00b439" CREATED="1631470819934" ID="ID_1808767559" MODIFIED="1631470823944" TEXT="Foto">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+</node>
+<node COLOR="#00b439" CREATED="1631470825424" ID="ID_790324085" MODIFIED="1631470827085" TEXT="Links">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+</node>
+<node COLOR="#00b439" CREATED="1631470827303" ID="ID_1845070490" MODIFIED="1631470830366" TEXT="Forma&#xe7;&#xe3;o">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+</node>
+<node COLOR="#00b439" CREATED="1631470834272" ID="ID_166818130" MODIFIED="1631470836526" TEXT="Atua&#xe7;&#xe3;o">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+</node>
+</node>
+<node COLOR="#0033ff" CREATED="1631469362349" ID="ID_1697364325" MODIFIED="1631485335708" POSITION="right" TEXT="Plano do Ensino">
+<edge STYLE="sharp_bezier" WIDTH="8"/>
+<font NAME="SansSerif" SIZE="18"/>
+<node COLOR="#00b439" CREATED="1631469589301" ID="ID_18360379" MODIFIED="1631469591013" TEXT="Ementa">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+</node>
+<node COLOR="#00b439" CREATED="1631485898184" ID="ID_416754796" MODIFIED="1631485904468" TEXT="Conte&#xfa;do Program&#xe1;tico">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+</node>
+<node COLOR="#00b439" CREATED="1631469591440" ID="ID_766618244" MODIFIED="1631469594523" TEXT="Metodologia de Ensino">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+</node>
+<node COLOR="#00b439" CREATED="1631469594741" ID="ID_1540950983" MODIFIED="1631469601864" TEXT="Metodologia de Avavalia&#xe7;&#xe3;o">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+</node>
+<node COLOR="#00b439" CREATED="1631469602171" ID="ID_1690298862" MODIFIED="1631478224011" TEXT="Bibliografia Sugerida">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+<node COLOR="#990000" CREATED="1631486312827" ID="ID_1943896794" MODIFIED="1631486322981" TEXT="Bibliografia B&#xe1;sica">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+<node COLOR="#990000" CREATED="1631486323198" ID="ID_1372367799" MODIFIED="1631486327181" TEXT="Bibliografia Complementar">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+</node>
+</node>
+<node COLOR="#0033ff" CREATED="1631469443486" ID="ID_1896496213" MODIFIED="1631490119913" POSITION="right" TEXT="Planejamento da Disciplina">
+<edge STYLE="sharp_bezier" WIDTH="8"/>
+<font NAME="SansSerif" SIZE="18"/>
+<node COLOR="#00b439" CREATED="1631469611841" ID="ID_1052073353" MODIFIED="1631469617824" TEXT="Calend&#xe1;rio Acad&#xea;mico">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+</node>
+<node COLOR="#00b439" CREATED="1631469619042" ID="ID_1426389575" MODIFIED="1631469620984" TEXT="Participa&#xe7;&#xe3;o">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+</node>
+<node COLOR="#00b439" CREATED="1631469621232" ID="ID_807859884" MODIFIED="1631469624574" TEXT="Lista de Exerc&#xed;cios">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+</node>
+<node COLOR="#00b439" CREATED="1631469629062" ID="ID_366179804" MODIFIED="1631469632425" TEXT="Semin&#xe1;rios">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+</node>
+<node COLOR="#00b439" CREATED="1631469632714" ID="ID_1839778364" MODIFIED="1631469634965" TEXT="Projetos">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+</node>
+<node COLOR="#00b439" CREATED="1631469624802" ID="ID_719927503" MODIFIED="1631469626815" TEXT="Avalia&#xe7;&#xf5;es">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+</node>
+<node COLOR="#00b439" CREATED="1631469639883" ID="ID_268197414" MODIFIED="1631469642806" TEXT="Comunica&#xe7;&#xe3;o da Turma">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+</node>
+</node>
+<node COLOR="#0033ff" CREATED="1631469453556" ID="ID_1369001382" MODIFIED="1631469546088" POSITION="right" TEXT="Introdu&#xe7;&#xe3;o da Disciplina">
+<edge STYLE="sharp_bezier" WIDTH="8"/>
+<font NAME="SansSerif" SIZE="18"/>
+<node COLOR="#00b439" CREATED="1631469705145" ID="ID_1038018437" MODIFIED="1631469708598" TEXT="T&#xf3;pico 1">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+</node>
+<node COLOR="#00b439" CREATED="1631469708935" ID="ID_314646593" MODIFIED="1631469711897" TEXT="T&#xf3;pico 2">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+</node>
+<node COLOR="#00b439" CREATED="1631469712515" ID="ID_1194670340" MODIFIED="1631484530981" TEXT="Exerc&#xed;cio">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+</node>
+</node>
+<node COLOR="#0033ff" CREATED="1631469674573" ID="ID_1905238469" MODIFIED="1631469796270" POSITION="right" TEXT="Tema 1">
+<edge STYLE="sharp_bezier" WIDTH="8"/>
+<font NAME="SansSerif" SIZE="18"/>
+<node COLOR="#00b439" CREATED="1631469727665" ID="ID_1195135506" MODIFIED="1631469732469" TEXT="T&#xf3;pico 1">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+</node>
+<node COLOR="#00b439" CREATED="1631469732996" ID="ID_1112644920" MODIFIED="1631469737878" TEXT="T&#xf3;pico 2">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+</node>
+<node COLOR="#00b439" CREATED="1631469738196" ID="ID_244993301" MODIFIED="1631484533450" TEXT="Exerc&#xed;cio">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+</node>
+</node>
+<node COLOR="#0033ff" CREATED="1631469685144" ID="ID_270163365" MODIFIED="1631469798690" POSITION="right" TEXT="Tema 2">
+<edge STYLE="sharp_bezier" WIDTH="8"/>
+<font NAME="SansSerif" SIZE="18"/>
+<node COLOR="#00b439" CREATED="1631469727665" ID="ID_1838752201" MODIFIED="1631469732469" TEXT="T&#xf3;pico 1">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+</node>
+<node COLOR="#00b439" CREATED="1631469732996" ID="ID_230433806" MODIFIED="1631469737878" TEXT="T&#xf3;pico 2">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+</node>
+<node COLOR="#00b439" CREATED="1631469738196" ID="ID_533860068" MODIFIED="1631484535403" TEXT="Exerc&#xed;cio">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+</node>
+</node>
+<node COLOR="#0033ff" CREATED="1631469689893" ID="ID_31002556" MODIFIED="1631469802719" POSITION="right" TEXT="Tema 3">
+<edge STYLE="sharp_bezier" WIDTH="8"/>
+<font NAME="SansSerif" SIZE="18"/>
+<node COLOR="#00b439" CREATED="1631469727665" ID="ID_983272704" MODIFIED="1631469732469" TEXT="T&#xf3;pico 1">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+</node>
+<node COLOR="#00b439" CREATED="1631469732996" ID="ID_1383635532" MODIFIED="1631469737878" TEXT="T&#xf3;pico 2">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+</node>
+<node COLOR="#00b439" CREATED="1631469738196" ID="ID_932881136" MODIFIED="1631484537431" TEXT="Exerc&#xed;cio">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+</node>
+</node>
+<node COLOR="#0033ff" CREATED="1631469689893" ID="ID_123341059" MODIFIED="1631469805861" POSITION="right" TEXT="Tema 4">
+<edge STYLE="sharp_bezier" WIDTH="8"/>
+<font NAME="SansSerif" SIZE="18"/>
+<node COLOR="#00b439" CREATED="1631469727665" ID="ID_1500731108" MODIFIED="1631469783930" TEXT="T&#xf3;pico 1">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+</node>
+<node COLOR="#00b439" CREATED="1631469732996" ID="ID_188191814" MODIFIED="1631469783930" TEXT="T&#xf3;pico 2">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+</node>
+<node COLOR="#00b439" CREATED="1631469738196" ID="ID_1413007583" MODIFIED="1631484540391" TEXT="Exerc&#xed;cio">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+</node>
+</node>
+<node COLOR="#0033ff" CREATED="1631469749587" ID="ID_316318478" MODIFIED="1631469756510" POSITION="right" TEXT="Lista de Exerc&#xed;cios 1">
+<edge STYLE="sharp_bezier" WIDTH="8"/>
+<font NAME="SansSerif" SIZE="18"/>
+</node>
+<node COLOR="#0033ff" CREATED="1631469749587" ID="ID_1852627079" MODIFIED="1631469818842" POSITION="right" TEXT="Lista de Exerc&#xed;cios 2">
+<edge STYLE="sharp_bezier" WIDTH="8"/>
+<font NAME="SansSerif" SIZE="18"/>
+</node>
+<node COLOR="#0033ff" CREATED="1631487139935" ID="ID_263344538" MODIFIED="1631487145978" POSITION="right" TEXT="Projeto 1">
+<edge STYLE="sharp_bezier" WIDTH="8"/>
+<font NAME="SansSerif" SIZE="18"/>
+</node>
+<node COLOR="#0033ff" CREATED="1631487147224" ID="ID_363765354" MODIFIED="1631487148939" POSITION="right" TEXT="Projeto 2">
+<edge STYLE="sharp_bezier" WIDTH="8"/>
+<font NAME="SansSerif" SIZE="18"/>
+</node>
+<node COLOR="#0033ff" CREATED="1631469693164" ID="ID_1518404298" MODIFIED="1631469696656" POSITION="right" TEXT="Agradecimento">
+<edge STYLE="sharp_bezier" WIDTH="8"/>
+<font NAME="SansSerif" SIZE="18"/>
+<node COLOR="#00b439" CREATED="1631469940188" ID="ID_518345184" MODIFIED="1631469947558" TEXT="Contatos">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+<node COLOR="#990000" CREATED="1631469948414" ID="ID_11368996" MODIFIED="1631469955306" TEXT="joaoferreirape@gmail.com">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+<node COLOR="#990000" CREATED="1631469955605" ID="ID_148480587" MODIFIED="1631469970950" TEXT="http://lattes.cnpq.br/8904695743376784">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+<node COLOR="#990000" CREATED="1631469979415" ID="ID_461249435" MODIFIED="1631469987617" TEXT="https://joaoferreirape.wordpress.com">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+</node>
+</node>
+<node COLOR="#0033ff" CREATED="1631469696934" ID="ID_345075612" MODIFIED="1631469700227" POSITION="right" TEXT="Refer&#xea;ncias">
+<edge STYLE="sharp_bezier" WIDTH="8"/>
+<font NAME="SansSerif" SIZE="18"/>
+</node>
+</node>
+</map>
